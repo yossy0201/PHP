7004b8466e02af5b4652453b305d0efd66738259
yossy0201 <yossy0201msk@gmail.com>
2020-09-19 23:24:44 +0900

    time-sentence

--- sample03.php
+++ sample03.php
@@ -12,14 +12,15 @@
 </head>
 <body>
 <header>
-<h1 class="font-weight-normal">PHP</h1>    
+<h1 class="font-weight-normal">PHP</h1>
 </header>
 
 <main>
 <h2>Practice</h2>
 <pre>
 <?php
-print(date('G時 i分 s秒'));
+print('現在は' . date('G時 i分 s秒'));
+print('ですです')
 ?>
 </pre>
 </main>
