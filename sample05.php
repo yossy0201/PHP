082da682662f8f05e84306426a4d29f4d96662a6
yossy0201 <yossy0201msk@gmail.com>
2020-09-20 00:02:52 +0900

    sample05-tax-calc

---
+++ sample05.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="css/style.css">
+
+<title>PHP</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">PHP</h1>    
+</header>
+
+<main>
+<h2>Practice</h2>
+<pre>
+<?php print((100+1050+200))*1.10;
+?>
+</pre>
+</main>
+</body>    
+</html>
